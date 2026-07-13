d1dc35d50e617adb02a2f7ae371e08c24a53abaf
Debjyoti Das <debjyoti@debjyoti.com>
2026-07-13 12:49:14 -0400

    login instruction updates and notification permission logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "com.eyediatech.eyedeeaphotos"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.9"
+        versionCode = 11
+        versionName = "1.10"
         vectorDrawables.useSupportLibrary = true
     }
 
