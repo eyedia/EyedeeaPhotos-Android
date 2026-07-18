2f4eeb87f4639b31328436df9e1235c016c0b4af
Debjyoti Das <debjyoti@debjyoti.com>
2026-07-18 19:17:03 -0400

    display app version in settings and update debug URLs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "com.eyediatech.eyedeeaphotos"
         minSdk = 21
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.12"
+        versionCode = 14
+        versionName = "1.13"
         vectorDrawables.useSupportLibrary = true
     }
 
@@ -94,10 +94,10 @@ android {
         getByName("debug") {
             isDebuggable = true
             applicationIdSuffix = ".debug"
-            buildConfigField("String", "BASE_URL", "\"http://192.168.86.100:5174/\"")
-            buildConfigField("String", "VIEW_URL", "\"http://192.168.86.100:5174/view\"")
-            buildConfigField("String", "LOGIN_URL", "\"http://192.168.86.100:5174/app-login?device=android\"")
-            buildConfigField("String", "VERIFICATION_URL", "\"http://192.168.86.100:5174/auth/device\"")
+            buildConfigField("String", "BASE_URL", "\"http://192.168.86.33:5174/\"")
+            buildConfigField("String", "VIEW_URL", "\"http://192.168.86.33:5174/view\"")
+            buildConfigField("String", "LOGIN_URL", "\"http://192.168.86.33:5174/app-login?device=android\"")
+            buildConfigField("String", "VERIFICATION_URL", "\"http://192.168.86.33:5174/activate\"")
             buildConfigField("boolean", "ENABLE_WEB_CONSOLE_LOG", "true")
         }
     }
